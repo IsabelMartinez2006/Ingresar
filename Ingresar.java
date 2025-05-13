3a24ff336f785530e2526d738bb77bde8e0828ba
IsabelMartinez2006 <20243rd025@utez.edu.mx>
2025-05-13 08:51:41 -0600

    Comparacion de cadenas: usuario y contraseña

---
+++ Ingresar.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Ingresar{
+
+    private static final Scanner scanner = new Scanner(System.in);//se declaro afuera para que fuera global 
+
+    public static String obtenerCadena(String mensaje){//Declara la funcio int que recibira como parametro una cadena 
+        String cadenaTemporal;
+        System.out.println(mensaje + ":");
+        cadenaTemporal = scanner.nextLine();
+        return cadenaTemporal;
+   
+    }
+
+    public static void main(String[] args ){
+
+        String nombreUsuario = "Isabel";
+        String contrasenaUsuario = "12345";
+        String usuarioIngresado = obtenerCadena("Ingresa tu usuario");
+        String contrasenaIngresada = obtenerCadena("Ingresa tu contraseña");
+
+        if ((nombreUsuario.equals(usuarioIngresado)) && (contrasenaIngresada.equals(contrasenaUsuario))){
+            System.out.println("Bienvenido ha iniciado sesion correctamente ");
+
+        }else{
+            System.out.println("Usuario o contraseña incorrecto");
+        }
+
+    }
+}
